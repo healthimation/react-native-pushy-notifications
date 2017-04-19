dfee788c0b6be4eedf6cb7e8badca083fba9a92e
Joe Leonard <joeleonard@gmail.com>
2017-04-19 13:52:16 -0300

    Ensure initialNotficaton boolean and userInteraction booleans are set on incoming notifications.

--- android/src/main/java/com/gijoehosaphat/RNPushyNotificationsReceiver.java
+++ android/src/main/java/com/gijoehosaphat/RNPushyNotificationsReceiver.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.util.Log;
 
 import static com.gijoehosaphat.RNPushyNotificationsModule.TAG;
+import static com.gijoehosaphat.RNPushyNotificationsModule.INITIAL_NOTIFICATION;
 import static com.gijoehosaphat.RNPushyNotificationsModule.USER_INTERACTION;
 
 public class RNPushyNotificationsReceiver extends BroadcastReceiver {
@@ -14,6 +15,7 @@ public class RNPushyNotificationsReceiver extends BroadcastReceiver {
   public void onReceive(Context context, Intent intent) {
     if (RNPushyNotificationsModule.isActive == true) { //Send event to app...
       intent.putExtra(USER_INTERACTION, false);
+      intent.putExtra(INITIAL_NOTIFICATION, false);
       RNPushyNotificationsModule.sendEvent(intent);
       Log.d(TAG, "onReceive: App is Active");
     } else { //Create tray notification...
